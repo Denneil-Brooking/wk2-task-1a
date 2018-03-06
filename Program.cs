2004ed5a2bb6d14a10eab5a3ed5f1be7cca16ae2
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2018-03-07 08:25:38 +1300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Start the program with Clear();
+           Console.Clear();
+           Console.WriteLine("------------------------------------------");
+           Console.WriteLine("This Program ");
+           Console.WriteLine("------------------------------------------");
+           
+           
+           //End the program with blank line and instructions
+           Console.ResetColor();
+           Console.WriteLine();
+           Console.WriteLine("**************PROGRAM END***************");
+           Console.WriteLine("Please Press <Enter> to quit the program");
+           Console.ReadKey();
+        }
+    }
+}
